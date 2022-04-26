5f003312372084a3faa4298fac5e96fcd9d9006c
MikeBTY <104447125+MikeBTY@users.noreply.github.com>
2022-04-26 19:16:17 +0200

    Added some solutions from students

--- K8/A3 var2.py
+++
@@ -1,7 +0,0 @@
-#Autokauf, Bedingung: 20 000€ , Klimaanlage
-Preis = int(input("Was kostet das Auto?"))
-Klimaanlage = str(input("Hat das Auto eine Klimaanlage?"))
-if Preis < 20000 and Klimaanlage == ("Ja") or Klimaanlage == ("JA"):
-    print("Kaufe das Auto.")
-else: 
-    print("Kaufe das Auto nicht.")
--- K9/A3_var2.py
+++
@@ -1,6 +0,0 @@
-def Countdown(x):
-    
-    while(x>0):
-        print(x)
-        x -= 1
-print(Countdown(10))
