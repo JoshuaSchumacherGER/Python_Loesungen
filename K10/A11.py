242448fd7a61ca6157306a403d695c251d6047d9
MikeBTY <104447125+MikeBTY@users.noreply.github.com>
2022-05-08 14:26:43 +0200

    Update A11.py

--- K10/A11.py
+++ K10/A11.py
@@ -1,8 +1,6 @@
 #K10|A11 Schreibe eine Funktion, die eine Liste als Parameter nimmt. Entweder wird das letzte Element der Liste
 #zurückgeliefert oder (falls die Liste leer sein sollte), wird der String „Leere Liste“ zurückgeliefert
 
-
-
 def checkliste():
     mylist = [2,4,5,123,4,31]
     if  len(mylist) == 0:
